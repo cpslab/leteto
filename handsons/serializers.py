69d27838d32274177b079c8c89e44eb812c7fa7e
h-cha <cocoa0331oreo@gmail.com>
2021-01-20 19:39:58 +0900

    feat(handson): add handson list api

---
+++ handsons/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import *
+
+# mserializer
+class HandsonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Handson
+        fields = ('id', 'owner', 'title', 'headline', 'detail', 'require', 'document_url', 'meeting_url', 'movie_url', 'start_at', 'end_at', 'created_at', 'updated_at', 'is_public', 'status')
--- handsons/views.py
+++ handsons/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import *
+from .serializers import HandsonSerializer
 
 # Create your views here.
+
+
+
+# Create your views here.
+class HandsonList(generics.ListCreateAPIView):
+    queryset = Handson.objects.all()
+    serializer_class = HandsonSerializer
