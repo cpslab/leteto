53a50888d5785b33867a28e8ec3d12e56bab1902
h-cha <cocoa0331oreo@gmail.com>
2021-01-28 12:17:47 +0900

    fix(handson): change class HandsonListSerializer fields in serializers

--- handsons/serializers.py
+++ handsons/serializers.py
@@ -3,7 +3,6 @@ from .models import Handson
 
 # Handson list
 class HandsonListSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Handson
         fields = ('id', 'owner', 'title', 'start_at', 'is_public', 'status')
