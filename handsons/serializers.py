2a4e33956ba92f5574f96415aef11f4673427995
h-cha <cocoa0331oreo@gmail.com>
2021-02-17 17:54:13 +0900

    feat(handson): add handson detail api

--- handsons/serializers.py
+++ handsons/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Handson
+from .models import Handson, CustomUser
+
+# User
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username')
 
 # Handson list
 class HandsonListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Handson
         fields = ('id', 'owner', 'title', 'start_at', 'is_public', 'status')
+
+# Handson detail
+class HandsonDetailSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
+    class Meta:
+        model = Handson
+        fields = ('id', 'owner', 'title', 'headline', 'detail', 'require', 'document_url', 'meeting_url', 'movie_url', 'start_at', 'is_public', 'status')
--- handsons/urls.py
+++ handsons/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HandsonList
+from .views import HandsonList, HandsonDetail
 
 urlpatterns = [
     path('', HandsonList.as_view()),
+        path('<int:id>', HandsonDetail.as_view()),
 ]
-
