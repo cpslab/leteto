c8e42005f7a2fbbd301d97448e8c13abb48ff920
h-cha <cocoa0331oreo@gmail.com>
2021-02-24 23:29:23 +0900

    fix: change end_at in models

--- handsons/models.py
+++ handsons/models.py
@@ -24,7 +24,7 @@ class Handson(models.Model):
 
     start_at = models.DateTimeField()
 
-    end_at = models.DateTimeField(null=True)
+    end_at = models.DateTimeField()
 
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
     
