8fb58010120562314adf5f1af746790e5a0c6365
h-cha <cocoa0331oreo@gmail.com>
2021-02-25 01:53:41 +0900

    fix(handson): change owner dealing with unique constraints in nested serializers

--- handsons/serializers.py
+++ handsons/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from .models import Handson, CustomUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 # User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username')
+        extra_kwargs = {
+            'username': {
+                'validators': [UnicodeUsernameValidator()],
+            }
+        }
 
 # Handson list & create
 class HandsonListCreateSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Handson
         fields = ('id', 'owner', 'title', 'headline', 'detail', 'require', 'document_url', 'meeting_url', 'movie_url', 'start_at', 'end_at', 'is_public', 'status')
@@ -41,6 +48,12 @@ class HandsonListCreateSerializer(serializers.ModelSerializer):
                 'write_only' : True,
             },
         }
+    def create(self, validated_data):
+        owner_data = validated_data.pop('owner')
+        username = owner_data.pop('username')
+        owner = CustomUser.objects.get_or_create(username=username)[0]
+        handson = Handson.objects.create(owner=owner, **validated_data)
+        return handson
 
 # Handson detail
 class HandsonDetailSerializer(serializers.ModelSerializer):
