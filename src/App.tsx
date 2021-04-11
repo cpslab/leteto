2a4b03e63c0813fdfe8952e98a87698e613b2623
narusei <naruse1wtbdev@gmail.com>
2021-04-11 14:02:03 +0900

    feat: add route setting

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,28 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import createTheam from './thema';
+import { AuthProvider } from './auth/AuthProvider';
+import { AuthenticatedRoute } from './auth/AuthenticatedRoute';
+import { PrivateRoute } from './auth/PrivateRoute';
+import { HandsonListPage } from './pages/handson/HandsonListPage';
+import { SignInPage } from './pages/SignInPage';
 
 const theme = createTheam();
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <AuthenticatedRoute exact path="/"></AuthenticatedRoute>
+          <Route exact path="/signin" component={SignInPage}></Route>
+          <PrivateRoute
+            exact
+            path="/handsons"
+            component={HandsonListPage}
+          ></PrivateRoute>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
