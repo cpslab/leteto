17b687f7faed8f88fb5990570d2c7682569377e5
narusei <naruse1wtbdev@gmail.com>
2021-04-13 19:30:16 +0900

    fix: header component props data

--- src/components/common/Header/Header.stories.tsx
+++ src/components/common/Header/Header.stories.tsx
@@ -13,7 +13,7 @@ const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  user: {},
+  user: { id: 'uniq-id' },
 };
 
 export const LoggedOut = Template.bind({});
