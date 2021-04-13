6ddf1a1d9dbb87423a01255ba8e38cc2af8e56ba
narusei <naruse1wtbdev@gmail.com>
2021-04-13 19:29:55 +0900

    feat: add sign in page component ui

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,5 +1,117 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import {
+  Avatar as MuiAvatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { Lock as LockOutlinedIcon } from '@material-ui/icons';
+
+const Paper = styled.div`
+  margin-top: ${(props) => props.theme.spacing(8)}px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LockAvator = styled(MuiAvatar)`
+  margin: ${(props) => props.theme.spacing(1)}px;
+  background-color: ${(props) => props.theme.palette.secondary.main};
+`;
+
+const Form = styled.form`
+  width: 100%;
+  margin-top: ${(props) => props.theme.spacing(1)}px;
+`;
+
+const SubmitButton = styled(Button)`
+  margin: ${(props) => props.theme.spacing(3, 0, 2)};
+`;
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+};
 
 export const SignInPage: React.FC = () => {
-  return <div></div>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Paper>
+        <LockAvator>
+          <LockOutlinedIcon />
+        </LockAvator>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <SubmitButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </SubmitButton>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Form>
+      </Paper>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 };
