2e31251a553057d819155de7bea284cb6eb65567
h-cha <cocoa0331oreo@gmail.com>
2021-04-30 00:43:39 +0900

    fix(handson): add users handson endpoint in urls

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HandsonListAPIView
 
 urlpatterns = [
-    path('<int:user_id>', HandsonListAPIView.as_view()),
+    path('<int:user_id>/handsons', HandsonListAPIView.as_view()),
 ]
