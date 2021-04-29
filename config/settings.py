42bdbf4895f58546162526f9c508affe5caaf4bc
h-cha <cocoa0331oreo@gmail.com>
2021-04-29 23:16:59 +0900

    feat(handson): add owner query

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     
     'rest_framework',
+    'django_filters',
 
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -144,7 +145,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),  
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 # core
--- handsons/views.py
+++ handsons/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 
-
+from users.models import CustomUser
 from .models import Handson, HandsonMember
 from .serializers import HandsonListCreateSerializer, HandsonRetrieveUpdateDestroySerializer, HandsonMemberSerializer
 from .permissions import IsAuthorOrReadOnly
+import django_filters.rest_framework as filters
+from rest_framework import exceptions
 
 # Create your views here.
 
 # Handson list & create
 
-
 class HandsonListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Handson.objects.all()
     serializer_class = HandsonListCreateSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned handsons to a given user,
+        by filtering against a `owner` query parameter in the URL.
+        """
+        queryset = Handson.objects.all()
+        owner_name = self.request.query_params.get('owner')
+        if owner_name is not None:
+            if owner_name == 'me':
+                current_user = self.request.user
+                queryset = queryset.filter(owner=current_user) 
+            else:
+                try:
+                    owner_name = CustomUser.objects.get(username=owner_name)
+                    queryset = queryset.filter(owner=owner_name)
+                except: 
+                    raise exceptions.ParseError('Invalid Request')
+        return queryset
 
 # Handson detail & update & delete
 
