3527eaa75ac31edb13fcdf545d86d1e82b1ed32c
mamyudapao <nosikenit@gmail.com>
2021-05-05 21:09:25 +0900

    feat(handson): add nested handson list view

--- handsons/urls.py
+++ handsons/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HandsonListCreateAPIView, HandsonRetrieveUpdateAPIView, HandsonMemberList, HandsonMemberRetrieveDestroyView
+from .views import HandsonListCreateAPIView, HandsonRetrieveUpdateAPIView, HandsonMemberList, HandsonMemberRetrieveDestroyView, NestedHandsonMember
 
 
 urlpatterns = [
     path('', HandsonListCreateAPIView.as_view()),
     path('<int:id>', HandsonRetrieveUpdateAPIView.as_view()),
+    path('<int:pk>/members', NestedHandsonMember.as_view()),
     path('handson_members/', HandsonMemberList.as_view()),
     path('handson_members/<int:pk>', HandsonMemberRetrieveDestroyView.as_view()),
 ]
