214fa48c9cc6822cfe339a22bf547794ba664dc1
narusei <naruse1wtbdev@gmail.com>
2021-05-19 22:00:16 +0900

    fix: modify app base component

--- src/components/common/AppBase/index.tsx
+++ src/components/common/AppBase/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Grid as MuiGrid } from '@material-ui/core';
+import { Grid as MuiGrid, Grid } from '@material-ui/core';
 import styled from 'styled-components/macro';
 
 const BackgroundGrid = styled(MuiGrid)`
   background-color: ${(props) => props.theme.palette.secondary.main};
-  width: 100%;
-  height: calc(100vh - 56px);
+  min-height: calc(100vh - 56px);
+  flex-grow: 1;
 `;
 
 export const AppBase: React.FC = (props) => {
-  return <BackgroundGrid>{props.children}</BackgroundGrid>;
+  return (
+    <BackgroundGrid container>
+      <Grid item xs={12}>
+        {props.children}
+      </Grid>
+    </BackgroundGrid>
+  );
 };
