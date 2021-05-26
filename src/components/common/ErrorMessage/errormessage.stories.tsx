a8d2713f8469176dd9ed7d95b7bb319033d6c36b
narusei <naruse1wtbdev@gmail.com>
2021-05-26 17:03:19 +0900

    feat: add error message component

---
+++ src/components/common/ErrorMessage/errormessage.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { ErrorMessage } from '.';
+
+export default {
+  title: 'component/common/ErrorMessage',
+  component: ErrorMessage,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story = (args) => <ErrorMessage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
