da8dffe7c427462b571d992f7e57fb888927df20
narusei <naruse1wtbdev@gmail.com>
2021-06-04 12:07:25 +0900

    fix: add changing background color feature to app main component

--- src/components/common/AppMain/index.tsx
+++ src/components/common/AppMain/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Paper as MuiPaper } from '@material-ui/core';
+import { Paper as MuiPaper, PropTypes } from '@material-ui/core';
 import { spacing } from '@material-ui/system';
 import styled from 'styled-components/macro';
 
+type AppMainProps = {
+  bgColor?: PropTypes.Color;
+};
+
 const Paper = styled(MuiPaper)(spacing);
 
-const MainContent = styled(Paper)`
+const MainContent = styled(Paper)<Pick<AppMainProps, 'bgColor'>>`
   flex: 1;
-  background-color: ${(props) => props.theme.palette.secondary.main};
+  background-color: ${(props) => {
+    switch (props.bgColor) {
+      case 'primary':
+        return props.theme.palette.primary.main;
+
+      case 'secondary':
+        return props.theme.palette.secondary.main;
+
+      case 'default':
+        return props.theme.palette.background.default;
+
+      default:
+        return props.theme.palette.secondary.main;
+    }
+  }};
 `;
 
-export const AppMain: React.FC = (props) => {
-  return <MainContent>{props.children}</MainContent>;
+export const AppMain: React.FC<AppMainProps> = (props) => {
+  return <MainContent bgColor={props.bgColor}>{props.children}</MainContent>;
 };
